154e519fe2790fdc79efae0820cbad69cf7d7759
rizamoyi <samwelmoyi@gmail.com>
2022-06-09 14:02:58 +0300

    Templating engines

--- controllers/messages.controllers.js
+++ controllers/messages.controllers.js
@@ -1,9 +1,10 @@
 const path = require('path');
 
 function getMessages(req, res) {
-  res.sendFile(
-    path.join(__dirname, '..', 'public', 'images', 'skimountain.jpg')
-  );
+  res.render('messages', {
+    title: 'Messages to my Friends!',
+    friend: 'Elon Musk',
+  });
 }
 
 function postMessage(req, res) {
